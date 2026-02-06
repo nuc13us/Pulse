47fd25ecccfc83616e8b5876750bee4d21e21d27
Rakesh Paruchuri <nuc13us@users.noreply.github.com>
2026-02-06 13:11:44 +0530

    Fix indentation and formatting in pulse.py

--- pulse.py
+++ pulse.py
@@ -23,9 +23,7 @@ for banner in api.search_cursor('html:/dana/'):
    if '200' in results:
        f = open('ips','a')
        f.write(str(banner['http']['host']) + "\n")
-
        print(str(banner['http']['host']))
    counter += 1
    if counter >= limit:
        break
-
